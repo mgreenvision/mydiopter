dc47b7f46dcc2bf05a9a1741b6bd8b61771ea1db
Dani Efrati <dani-e@drorisrael.org.il>
2022-10-23 22:58:46 +0300

    basic ASTG test layout

--- src/Pages/AstgTest/AstgTest.jsx
+++ src/Pages/AstgTest/AstgTest.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 import Navbar from "../../Components/Navbar/Navbar";
 
 function AstgTest() {
   return (
-    <div>
-      <Navbar topic="ASTG" />
-      AstgTest
-    </div>
+    <ASTGLayout>
+      <NavbarPlace topic="ASTG" />
+      <DataWrapper />
+      <StarWrapper />
+      <ButtonWrapper />
+    </ASTGLayout>
   );
 }
 
 export default AstgTest;
+
+const ASTGLayout = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 100vw;
+  grid-template-rows: var(--navbar-height) 20vh 100vw auto;
+  grid-template-areas:
+    " navbar "
+    " click-data "
+    " star "
+    " button ";
+`;
+
+const NavbarPlace = styled(Navbar)`
+  grid-area: navbar;
+`;
+
+const DataWrapper = styled.div`
+  grid-area: click-data;
+  place-items: center;
+  background-color: darkgoldenrod;
+`;
+
+const StarWrapper = styled.div`
+  grid-area: star;
+  place-items: center;
+  background-color: crimson;
+`;
+
+const ButtonWrapper = styled.div`
+  grid-area: button;
+  place-items: center;
+  background-color: lavender;
+`;
