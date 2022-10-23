bff4114058889b56dd3bbd639dc9796d9bbef290
Dani Efrati <dani-e@drorisrael.org.il>
2022-10-23 20:40:00 +0300

    dichotomy page layout

--- src/Components/branded.jsx
+++ src/Components/branded.jsx
@@ -1,9 +1,26 @@
 import styled from "styled-components";
 
 export const NavbarIcon = styled.img`
-  object-fit: contain;
   height: 22px;
   margin-left: ${(props) => props.left};
   margin-right: ${(props) => props.right};
-  border: ${(props) => (props.border ? "solid 2px white" : "none")};
+`;
+
+export const HeaderText = styled.h1`
+  text-align: center;
+  font-family: var(--font-topic);
+  font-size: var(--text-size-h1);
+  font-weight: 700;
+  color: var(--color-text);
+`;
+
+export const ColorBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /*   background-color: rgb(
+    ${(props) => props.red},
+    ${(props) => props.green},
+    ${(props) => props.blue}
+  ); */
 `;
--- src/Pages/DichotomyTest/DichotomyTest.jsx
+++ src/Pages/DichotomyTest/DichotomyTest.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "../../Components/Navbar/Navbar";
+import { ColorBox } from "../../Components/branded";
+import { HeaderText } from "../../Components/branded";
 
 function DichotomyTest() {
-	return <div>DichotomyTest</div>;
+  return (
+    <DichotomyLayout>
+      <NavbarPlace topic="DICHOTOMY" />
+      <CTA>
+        <HeaderText>Pick stronger color</HeaderText>
+      </CTA>
+      <UpperColorBox red="200" green="200" blue="200" />
+      <LowerColorBox red="100" green="100" blue="100" />
+    </DichotomyLayout>
+  );
 }
 
 export default DichotomyTest;
+
+const DichotomyLayout = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 100vw;
+  grid-template-rows: var(--navbar-height) auto 1fr 1fr;
+  grid-template-areas:
+    " navbar "
+    " cta "
+    " upper-color "
+    " lower-color ";
+`;
+
+const NavbarPlace = styled(Navbar)`
+  grid-area: navbar;
+`;
+
+const CTA = styled.div`
+  grid-area: cta;
+  display: flex;
+  flex-direction: row;
+  place-items: center;
+`;
+
+const UpperColorBox = styled(ColorBox)`
+  grid-area: upper-color;
+  background-color: #cecece;
+  place-items: center;
+`;
+
+const LowerColorBox = styled(ColorBox)`
+  grid-area: lower-color;
+  background-color: #767676;
+  place-items: center;
+`;
